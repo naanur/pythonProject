08852449c1b0762876a89cdbc51e1eec212a1d75
naanur <n@mail.kz>
2022-09-03 14:46:44 +0600

    fixes

--- TestProject/TestProject/settings.py
+++ TestProject/TestProject/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'app.apps.MainappConfig',
+    'app.apps.AppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
